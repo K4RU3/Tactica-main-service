43ea97116c168ee5dcf28b96a3b0acae5a50ce2b
K4RU3 <98632450+K4RU3@users.noreply.github.com>
2024-02-22 10:10:51 +0900

    盛者必衰

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express"
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("hello world")
+  res.send("盛者必衰の理を表す")
 });
 
 const port = parseInt(process.env.PORT) || 8080;
