bfc147183619cbd2aaa0f4adb8e45e117fb903c4
K4RU3 <98632450+K4RU3@users.noreply.github.com>
2024-02-23 21:04:10 +0900

    test

--- app/main.cpp
+++ app/main.cpp
@@ -1,19 +1,17 @@
 #include "httplib.h"
 #include <iostream>
-#include <vector>
-#include <mutex>
 #include <string>
 
 using namespace std;
 
 void handleGet(const httplib::Request& req, httplib::Response& res){
-    res.send("test")
+    res.send("test", "text/plain")
 }
 
 int main(){
     httplib::Server app;
 
-    app.Get('/', handleGet);
+    app.Get("/", handleGet);
 
     app.listen("0.0.0.0", 8080);
 
