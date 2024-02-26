a43406ce89575a6497b702c614f1619cfc9f779e
K4RU3 <pzKARUzq2@gmail.com>
2024-02-26 14:14:48 +0900

    sse

--- app/main.cpp
+++ app/main.cpp
@@ -29,8 +29,29 @@ int main(){
             }
             res.set_content(value, "text/plain");
         }catch(const exception& e){
-            res.set_content("Invlid the body", "text/plain");
+            res.set_content(uuidGenerator.getUUID().str(), "text/plain");
         }
+    });
+
+    app.Get("/sse", [](const Request& req, Response& res){
+        int count;
+        try{
+            count = stoi(req.get_param_value("count"));
+        }catch(const exception& e){
+            count = 10;
+        }
+
+        res.set_chunked_content_provider(
+            "test/plain",
+            [&count](size_t offset, DataSink &sink){
+                for(int i = 0; i < count; i++){
+                    this_thread::sleep_for(chrono::seconds(1));
+                    string uuid = uuidGenerator.getUUID().str();
+                    sink.write(uuid.c_str(), uuid.size());
+                }
+                return true;
+            }
+        );
     });
 
     thread server([&app](){
