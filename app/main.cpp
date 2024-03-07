0360f1bd750417edcfc811ea0dcb8128a4c49f96
K4RU3 <pzKARUzq2@gmail.com>
2024-03-07 15:01:49 +0900

    json and delete

--- app/main.cpp
+++ app/main.cpp
@@ -1,20 +1,7 @@
 #include <iostream>
-#include <thread>
-
 #include <uuid_v4.h>
+#include <nlohmann/json.hpp>
 
-#include "Server.cpp"
-
-using namespace std;
-
-//定義域
-UUIDv4::UUIDGenerator<mt19937_64> uuidGenerator;
-
-/* ===== main ===== */
-int main(){
-    Server server;
-
+int main(void){
     
-
-    return 0;
 }
